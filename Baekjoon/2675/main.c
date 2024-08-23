df2499896ac4ca4ac161698e18db2e1ee097c4ce
Nahyun-Kim <50420981+NahyunEE@users.noreply.github.com>
2024-08-23 22:45:23 +0900

    2675

--- Baekjoon/2675/main.c
+++ Baekjoon/2675/main.c
@@ -1,52 +1,36 @@
 #include <stdio.h>
 #include <stdlib.h>
+#include <string.h> 
 
-/* run this program using the console pauser or add your own getch, system("pause") or input loop */
-
-
-
-
-struct TestCase{
-	int number;
-	char word[20];
-	
+struct Point {
+    int num;
+    char word[21];
 };
 
 int main(int argc, char *argv[]) {
-	
-	int Test;
-	struct TestCase * arr;
-	int i=0;
-	int j=0;
-	int n=0;
+    int testcase;
+    int i=0;
+	int j=0; 
 	int k=0;
-	
-	arr=(struct TestCase*)malloc(sizeof(struct TestCase)*Test);	
-	
-	scanf("%d",Test);
-	getchar();
-	
-	
-	
-	for(i=0; i< Test; i++){		
-		scanf("%d %s", &arr[i].number, arr[i].word);	
-			
-	}
-	
-	for(j=0; j<Test; j++){ // 각각의 테스트케이스를 읽기 
-		struct TestCase temp_tt=arr[j];
-	
-		for(k=0; k < strlen(temp_tt.word) ;++k){ //
-			for(n=0,l=0; n<temp_tt.number;++n,++l){
-				printf("%c",temp_tt.word[l];
-			}
-		}	
-		
-	}
-	
-	free(arr);
-	
-	
-	
-	return 0;
+    scanf("%d", &testcase);
+    
+    struct Point p[1000];
+    
+    for(i = 0; i < testcase; i++) {
+        scanf("%d %s", &p[i].num, p[i].word);
+    }
+    
+    for(i = 0; i < testcase; i++) {
+        int wordsize = strlen(p[i].word); 
+        
+        for(j = 0; j < wordsize; j++) {
+            for(k = 0; k < p[i].num; k++) {
+                printf("%c", p[i].word[j]);
+            }
+        }
+        printf("\n"); 
+    }
+    
+    return 0;
 }
+
